e3b0fac4d56039ef8dc1e717c46cdfcf6d03f801
ZaxonXP <ZaxonXP@users.noreply.github.com>
2020-09-22 14:14:27 +0200

    Main script which plays the saved playlist.

---
+++ gp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export PATH=$PATH:$HOME/Batch
+
+if [[ "$1" == "-o" ]]; then
+    gvim_minimal.sh -o $2 > /dev/null 2>&1 
+    shift
+else
+    gvim_minimal.sh $1 > /dev/null 2>&1 
+fi
+youtube-dl --rm-cache-dir
+
+name=$(echo "$1" | cut -d'.' -f1 | rev | cut -d"/" -f1 | rev)
+main=$((xdotool search --class xterm ; xdotool search --name $name) | sort | uniq -d)
+wid=$((xdotool search --class Gvim ; xdotool search --name $name) | sort | uniq -d)
+
+xdotool windowmove $wid 960 120
+wmctrl -ir $main -b remove,maximized_vert,maximized_horz
+xdotool windowsize $main 958 1056
+gapless_play.sh $1
