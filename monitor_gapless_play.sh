75063777513d7cf1f798cf7528cad1dbc92dcc66
ZaxonXP <ZaxonXP@users.noreply.github.com>
2020-09-22 14:20:55 +0200

    Monitor gapless playback

--- monitor_gapless_play.sh
+++ monitor_gapless_play.sh
@@ -2,17 +2,19 @@
 
 # Shows currently played song when used with gapless_play.sh
 source="$1"
-tracks=$(wc -l "$source" | cut -d" " -f1)
 
 clear
 while [ 1 ]; do
 
+    # get always updated track info
+    tracks=$(wc -l "$source" | cut -d" " -f1)
+
     # get oldest mpv process, youtube ID and process ID
-    mpv_proc=$(ps aux --sort start_time | grep -v grep | grep -v "youtube-viewer" | grep mpv | head -1)
+    mpv_proc=$(ps aux --sort start_time | grep -v grep | grep -v "youtube-viewer" | grep "/tmp/soc" | grep mpv | head -1)
 
     if [[ "$mpv_proc" != "" ]]; then
 
-        yt_id=$(echo $mpv_proc | cut -d= -f3)
+        yt_id=$(echo $mpv_proc | cut -d= -f4)
         mpv_id=$(echo $mpv_proc | tr -s " " | cut -d" " -f2)
 
         first=${yt_id:0:1}
@@ -20,23 +22,29 @@ while [ 1 ]; do
         if [[ "$first" == "-" ]]; then
 
             info=$(grep \\$yt_id "$source")
-            track=$(grep -n \\$yt_id "$source" | cut -d: -f1)
+            track=$(grep -n \\$yt_id "$source" | head -1 | cut -d: -f1)
         else
             info=$(grep $yt_id "$source")
-            track=$(grep -n $yt_id "$source" | cut -d: -f1)
+            track=$(grep -n $yt_id "$source" | head -1 | cut -d: -f1)
         fi
 
         if [ -v DEBUG ]; then
+            echo "SOURCE = \"$source\""
             echo "YT ID  = \"$yt_id\""
             echo "MPV ID = \"$mpv_id\""
             echo "INFO   = \"$info\""
+            echo "FIRST  = \"$first\""
+            echo "PROC   = \"$mpv_proc\""
         fi
 
+        echo "File   :  $1"
         echo "Artist : $(echo $info | cut -d\| -f3)"
         echo "Title  : $(echo $info | cut -d\| -f4)"
         echo "Time   :  $(echo $info | cut -d\| -f1)"
         echo "Track  :  $track / $tracks"
         echo "YT ID  : $(echo $info | cut -d\| -f2)"
+
+        gvim --servername $(basename $1) --remote-send ":${track}<cr><home>"
 
         # wait until the process exit to iterate
         tail --pid=$mpv_id -f /dev/null
